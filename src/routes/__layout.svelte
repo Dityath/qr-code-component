bcf45e7a85ac68e0b9b34764614a8b41e518bcf9
dityath <dityaathallah107@gmail.com>
2022-04-28 14:05:27 +0700

    fix #1

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,13 +4,11 @@
 
 <head>
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="true" />
 	<link
 		href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&display=swap"
 		rel="stylesheet"
 	/>
-
-	<title>QR code component</title>
 </head>
 
 <main class="bg-light-gray h-screen w-screen flex justify-center items-center">
